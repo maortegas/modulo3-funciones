21b0190bbc12b6f6680bd46be7ca0546657d7f41
Mauricio Ortega <mauricio.ortega.solis@gmail.com>
2023-07-18 20:53:21 -0400

    Add 1_funcion.js

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,12 @@
+/*
+function example(a, b, c){ 
+    return a+b+c
+}
+ */
+
+example = function(a, b, c){ 
+    return a+b+c
+}
+ 
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+
+example= function(a, b, c){ 
+    return a+b+c
+}
+ 
+console.log(example(1,2,3));
+
